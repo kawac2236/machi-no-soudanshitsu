cdcaa96762d6b5f9ff18e9aa29d67e5666b0e790
kawac2236 <kawac2236@gmail.com>
2019-10-12 14:27:14 +0900

    RSpecのパフォーマンス・チューニング。dockerのvolumeマウントをcachedに変更。rails 6.0.0にアップデート refs #11

--- src/spec/controllers/home_controller_spec.rb
+++ src/spec/controllers/home_controller_spec.rb
@@ -5,28 +5,28 @@ RSpec.describe HomeController, type: :controller do
   describe "root" do
     it "returns http success" do
       get :top
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status 200
     end
   end
 
   describe "GET #top" do
     it "returns http success" do
       get :top
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
     end
   end
 
   describe "GET #about" do
     it "returns http success" do
       get :about
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
     end
   end
 
   describe "GET #help" do
     it "returns http success" do
       get :help
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
     end
   end
 
