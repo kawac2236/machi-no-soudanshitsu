709a7e4b22c6081dbf485060579eaa87058a9cc1
kawac2236 <kawac2236@gmail.com>
2019-10-12 17:16:15 +0900

    Rubocopの導入とフォーマット　refs #11

--- src/test/controllers/home_controller_test.rb
+++ src/test/controllers/home_controller_test.rb
@@ -1,30 +1,27 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-
-  test "ルートURLのテスト" do
+  test 'ルートURLのテスト' do
     get root_url
     assert_response :success
-    assert_select "title", "まちの相談室"
+    assert_select 'title', 'まちの相談室'
   end
 
-  test "/topのテスト" do
+  test '/topのテスト' do
     get home_top_url
     assert_response :success
-    assert_select "title", "まちの相談室"
+    assert_select 'title', 'まちの相談室'
   end
 
-  test "/helpのテスト" do
+  test '/helpのテスト' do
     get home_help_url
     assert_response :success
-    assert_select "title", "ヘルプ"
-
+    assert_select 'title', 'ヘルプ'
   end
 
-  test "/aboutのテスト" do
+  test '/aboutのテスト' do
     get home_about_url
     assert_response :success
-    assert_select "title", "まちの相談室について"
+    assert_select 'title', 'まちの相談室について'
   end
-
 end
