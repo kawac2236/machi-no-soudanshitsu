14c495e8eedbaf342d2dd69bf0ee5997bc5feaa1
kawac2236 <kawac2236@gmail.com>
2019-10-12 15:20:20 +0900

    SCSSの追加とタイトルのテストの追加 refs #11

--- src/spec/controllers/home_controller_spec.rb
+++ src/spec/controllers/home_controller_spec.rb
@@ -3,30 +3,34 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
 
   describe "root" do
-    it "returns http success" do
+    it "HTTPリクエストとタイトル" do
       get :top
       expect(response).to have_http_status 200
+      assert_select "title", "まちの相談室"
     end
   end
 
   describe "GET #top" do
-    it "returns http success" do
+    it "HTTPリクエストとタイトル" do
       get :top
       expect(response).to have_http_status 200
-    end
-  end
-
-  describe "GET #about" do
-    it "returns http success" do
-      get :about
-      expect(response).to have_http_status 200
+      assert_select "title", "まちの相談室"
     end
   end
 
   describe "GET #help" do
-    it "returns http success" do
+    it "HTTPリクエストとタイトル" do
       get :help
       expect(response).to have_http_status 200
+      assert_select "title", "ヘルプ"
+    end
+  end
+
+  describe "GET #about" do
+    it "HTTPリクエストとタイトル" do
+      get :about
+      expect(response).to have_http_status 200
+      assert_select "title", "まちの相談室について"
     end
   end
 
